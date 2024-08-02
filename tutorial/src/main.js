137ed3866480dc2377f5b61bf9dabc62a91f918e
M0RT4L48 <147433876+M0RT4L48@users.noreply.github.com>
2024-08-02 16:03:50 -0600

    final main.js

--- tutorial/src/main.js
+++ tutorial/src/main.js
@@ -1,4 +1,43 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+createApp({
+  data() {
+    return {
+      titulo: 'Hola Mundo con Vue',
+      frutas: [
+        { nombre: 'Pera', cantidad: 10 },
+        { nombre: 'Manzana', cantidad: 0 },
+        { nombre: 'Platano', cantidad: 11 }
+      ],
+      nuevaFruta: ''
+    };
+},
+    methods:{
+        agregarFruta(){
+        this.frutas.push({
+            nombre: this.nuevaFruta, cantidad: 0
+        });
+        this.nuevaFruta = '';
+    }
+  },
+  computed: {
+    sumarFrutas() {
+      let total = 0;
+      for (const fruta of this.frutas) {
+        total += fruta.cantidad;
+      }
+      return total;
+    }
+  }
+  
+  //cambia la logica
+  //computed:{
+    //sumarFrutas(){
+      //this.total = 0;
+      //for(fruta of this.frutas){
+       // this.total = this.total + fruta.cantidad;
+      //}
+     // return this.total;
+    //}
+  //}
+}).mount('#app');
